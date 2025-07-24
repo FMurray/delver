3fdc008769d9e782c9b1791c48dd0ebf902fd486
Forrest Murray <forrest.murray@databricks.com>
2025-07-24 12:12:47 -0400

    loading sequence order with generated js script & pdfium

--- crates/viewer/dist/50e37bd8f5b803ba-initializer.js
+++
@@ -1,9 +0,0 @@
-export default async function() {
-    const pdfium = await window.PDFiumModule();
-
-    return {
-        onSuccess: (wasm) => {
-            wasm.initialize_pdfium_render(pdfium, wasm);
-        }
-    }
-} 
